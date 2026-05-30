7c4dd9f745df19142cda7caa96fad2c882820823
zesdz3413-a11y <zesdz3413@gmail.com>
2026-05-30 19:10:16 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,49 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
-
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
-using namespace geode::prelude;
-
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
 #include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+#include <cocos2d.h>
+#include "ui/CocosGUI.h" 
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+using namespace geode::prelude;
+using namespace cocos2d::ui;
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+        // 1. An hinh nen mac dinh cua game
+        if (auto background = this->getChildByID("main-menu-bg")) {
+            background->setVisible(false);
+        } else if (this->getChildrenCount() > 0) {
+            auto firstChild = typeinfo_cast<CCSprite*>(this->getChildren()->objectAtIndex(0));
+            if (firstChild) firstChild->setVisible(false);
+        }
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+        // 2. Lay kich thuoc man hinh
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+        // 3. Tao Trinh phat Video
+        auto videoPlayer = VideoPlayer::create();
+        videoPlayer->setContentSize(winSize);
+        videoPlayer->setPosition(ccp(winSize.width / 2, winSize.height / 2));
+        videoPlayer->setAnchorPoint(ccp(0.5f, 0.5f));
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+        // 4. Goi file video da khai bao trong mod.json
+        videoPlayer->setFileName("menu_bg.mp4");
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+        // 5. Cau hinh phat va lap lai (Loop)
+        videoPlayer->setKeepAspectRatioEnabled(true);
+        videoPlayer->setFullScreenEnabled(false);
+        
+        videoPlayer->addEventListener([videoPlayer](CCObject* sender, VideoPlayer::EventType eventType) {
+            if (eventType == VideoPlayer::EventType::COMPLETED) {
+                videoPlayer->play(); 
+            }
+        });
+
+        // 6. Dua video ra sau cung (Z-order = -1) va phat
+        this->addChild(videoPlayer, -1);
+        videoPlayer->play();
+
+        return true;
+    }
 };
